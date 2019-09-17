1a83079c1eb88ef83bd675cd23691ba39914e713
Gray Hound <admin@devilmaydie.name>
2019-09-17 18:01:30 +0500

    Additional tests with tokens

--- src/tests/Profile.js
+++ src/tests/Profile.js
@@ -9,14 +9,15 @@ chai.use(chaiHttp);
 chai.use(chaiSubset);
 
 export default function Profile(app, options = {}) {
-  let token;
+  let accessToken;
+  let refreshToken;
+  let newAccessToken;
 
   describe('Profile endpoints', () => {
     describe('POST /profile', () => {
       it('should get unauthorized', (done) => {
         chai.request(app.server)
           .get(`${options.apiPrefix}/profile`)
-          .set('Authorization', 'Bearer ' + token)
           .send()
           .end((err, res) => {
             res.should.have.status(401);
@@ -37,7 +38,9 @@ export default function Profile(app, options = {}) {
             res.should.have.status(200);
             res.body.should.be.a('object');
             res.body.should.have.property('token');
-            token = res.body.token;
+            res.body.should.have.property('refreshToken');
+            accessToken = res.body.token;
+            refreshToken = res.body.refreshToken;
             done();
           });
       });
@@ -45,13 +48,53 @@ export default function Profile(app, options = {}) {
       it('should get a profile', (done) => {
         chai.request(app.server)
           .get(`${options.apiPrefix}/profile`)
-          .set('Authorization', 'Bearer ' + token)
+          .set('Authorization', 'Bearer ' + accessToken)
           .send()
           .end((err, res) => {
             res.should.have.status(200);
             res.body.should.be.a('object');
             res.body.should.have.property('email').eql('test2@test.com');
-            token = res.body.token;
+            done();
+          });
+      });
+
+      it('should get new tokens using refreshToken', (done) => {
+        chai.request(app.server)
+          .post(`${options.apiPrefix}/auth/user/token/refresh`)
+          .send()
+          .set('Authorization', `Bearer ${refreshToken}`)
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('token');
+            res.body.should.have.property('refreshToken');
+            newAccessToken = res.body.token;
+            done();
+          });
+      });
+
+      it('should get a profile with new token', (done) => {
+        chai.request(app.server)
+          .get(`${options.apiPrefix}/profile`)
+          .set('Authorization', 'Bearer ' + newAccessToken)
+          .send()
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('email').eql('test2@test.com');
+            done();
+          });
+      });
+
+      it('should get a profile with old token', (done) => {
+        chai.request(app.server)
+          .get(`${options.apiPrefix}/profile`)
+          .set('Authorization', 'Bearer ' + accessToken)
+          .send()
+          .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+            res.body.should.have.property('email').eql('test2@test.com');
             done();
           });
       });
